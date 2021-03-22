8b072bde209f9dbcec71deb00d5812982ec3c6b6
GennyBoy <tassie2856@gmail.com>
2021-03-23 06:46:10 +0900

    case文の方が今回の（1つの変数の値に応じて、複数の処理から１つを選んで実行する）課題には適していると考え修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,11 +2,12 @@
 
 20.times do |i|
   i += 1
-  if i % 3 == 0 && i % 5 == 0
+  case
+  when i % 3 == 0 && i % 5 == 0
     puts "FizzBuzz"
-  elsif i % 3 == 0
+  when i % 3 == 0
     puts "Fizz"
-  elsif i % 5 == 0
+  when i % 5 == 0
     puts "Buzz"
   else
     puts i
