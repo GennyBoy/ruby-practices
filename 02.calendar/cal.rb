36d1451755adce8753fec20322108d2eba14b3e5
GennyBoy <tassie2856@gmail.com>
2021-04-11 10:17:05 +0900

    rubocop-fjordを実行して、違反があった点をauto-correctした

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
-params = ARGV.getopts("m:", "y:")
+params = ARGV.getopts('m:', 'y:')
 
 # short_optにはデフォルト値を設定できないので、
 # nilの(引数が渡されなかった)場合に今月の年月を設定する
@@ -14,9 +16,9 @@ END_DATE = Date.new(year, month, -1)
 END_DAY = END_DATE.day
 
 puts "   　　#{month}月 #{year}"
-puts "日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 
-print "   " * START_DATE.wday
+print '   ' * START_DATE.wday
 (START_DATE..END_DATE).each do |date|
   day = date.day.to_s
   # 今日の日付であれば、背景色と文字色を反転させる
@@ -25,7 +27,7 @@ print "   " * START_DATE.wday
   else
     print day.rjust(2)
   end
-  print " "
+  print ' '
   print "\n" if date.saturday?
 end
 print "\n"
